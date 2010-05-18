4a9deb1378fd90a47bad06b9b6e91fc928b5b1cd
Martin Kühl <martin.kuehl@gmail.com>
2010-05-19 01:18:40 +0200

    Update to the latest QSB beta API.
    Basically a rewrite that uses asynchronous operations and some more Vermilion features.
    Some restructuring as well.

--- FastScriptsSource.m
+++ FastScriptsSource.m
@@ -20,147 +20,208 @@ static NSString *const kFSURIFormat = @"FastScripts://FastScripts/%@";
 static NSString *const kFSInvokeAction
   = @"org.purl.net.mkhl.FastScripts.action.invoke";
 
+#pragma mark Helpers
+static FastScriptsApplication *_FSApp(void) {
+  return [SBApplication applicationWithBundleIdentifier:kFSBundleIdentifier];
+}
+
+static NSString *_FSAppPath(void) {
+  NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
+  return [workspace absolutePathForAppBundleWithIdentifier:kFSBundleIdentifier];
+}
+
+static NSString *_FSItemURI(NSString *name, NSString *path) {
+  path = [path stringByAppendingPathComponent:name];
+  return [NSString stringWithFormat:kFSURIFormat,
+          [path stringByAddingPercentEscapesUsingEncoding:
+           NSUTF8StringEncoding]];
+}
+
+static NSString *_FSItemApp(NSArray *pathComponents) {
+  NSString *app = nil;
+  if ([pathComponents count] > 1) {
+    if ([[pathComponents objectAtIndex:0] isEqualToString:@"Applications"]) {
+      app = [pathComponents objectAtIndex:1];
+    }
+  }
+  return app;
+}
+
+static HGSAction *_FSItemAction(void) {
+  HGSExtensionPoint *point = [HGSExtensionPoint actionsPoint];
+  return [point extensionWithIdentifier:kFSInvokeAction];
+}
+
 #pragma mark -
 @interface FastScriptsSource : HGSMemorySearchSource {
  @private
-  NSImage *appIcon_;
+  NSString *appPath_;
+  NSOperation *indexingOperation_;
 }
-- (void)recacheContents;
-- (void)recacheContentsAfterDelay:(NSTimeInterval)delay;
-- (void)indexScriptItem:(FastScriptsScriptItem *)script
-   atPathWithComponents:(NSArray *)pathComponents;
-- (void)indexScriptLibrary:(FastScriptsScriptLibrary *)library
-      atPathWithComponents:(NSArray *)pathComponents;
-- (void)indexTopLevelScriptLibrary:(FastScriptsScriptLibrary *)library;
+- (void)reloadResultsCache;
+- (void)indexScriptsForApp:(FastScriptsApplication *)application
+                         operation:(NSOperation *)operation;
 @end
 
 #pragma mark -
 @implementation FastScriptsSource
 
 #pragma mark Memory Management
-- (id)initWithConfiguration:(NSDictionary *)configuration
-{
-  self = [super initWithConfiguration:configuration];
-  if (self) {
-    NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
-    NSString *bundlePath
-      = [workspace absolutePathForAppBundleWithIdentifier:kFSBundleIdentifier];
-    if (bundlePath == nil) {
-      [self release];
-      return nil;
+- (id)initWithConfiguration:(NSDictionary *)configuration {
+  if ((self = [super initWithConfiguration:configuration])) {
+    if ((appPath_ = [_FSAppPath() retain]) == nil) {
+      HGSLog(@"%@: Unable to locate FastScripts.app", self);
     }
-    appIcon_ = [[workspace iconForFile:bundlePath] retain];
-    if ([self loadResultsCache]) {
-      [self recacheContentsAfterDelay:10.0];
-    } else {
-      [self recacheContents];
-    }
+    [self reloadResultsCache];
   }
   return self;
 }
 
-- (void)dealloc
-{
-  DESTROY(appIcon_);
+- (void)dealloc {
+  [appPath_ release];
+  [indexingOperation_ release];
   [super dealloc];
 }
 
-#pragma mark Result Index
-- (void)recacheContents
-{
-  [self clearResultIndex];
-  FastScriptsApplication *app
-    = [SBApplication applicationWithBundleIdentifier:kFSBundleIdentifier];
-  for (FastScriptsScriptLibrary *library in [app topLevelScriptLibraries]) {
-    [self indexTopLevelScriptLibrary:library];
-  }
-  [self saveResultsCache];
-  [self recacheContentsAfterDelay:60.0];
+- (void)uninstall {
+  [indexingOperation_ cancel];
+  [super uninstall];
 }
 
-- (void)recacheContentsAfterDelay:(NSTimeInterval)delay
-{
-  SEL action = @selector(recacheContents);
-  [self performSelector:action withObject:nil afterDelay:delay];
+- (void)updateAppPath {
+  if (appPath_ == nil) {
+    appPath_ = [_FSAppPath() retain];
+  }
+}
+
+#pragma mark Result Index
+- (void)updateResultsCache {
+  [indexingOperation_ cancel];
+  [indexingOperation_ release];
+  SEL update = @selector(updateResultsWithDummy:operation:);
+  indexingOperation_ = [[NSInvocationOperation alloc] hgs_initWithTarget:self
+                                                                selector:update
+                                                                  object:nil];
+  [[HGSOperationQueue sharedOperationQueue] addOperation:indexingOperation_];
+}
+
+- (void)updateResultsCacheAfterDelay:(NSTimeInterval)delay {
+  [self performSelector:@selector(updateResultsCache)
+             withObject:nil
+             afterDelay:delay];
+}
+
+- (void)updateResultsWithDummy:(id)dummy operation:(NSOperation *)operation {
+  if (![operation isCancelled]) {
+    FastScriptsApplication *app = _FSApp();
+    if (app) {
+      [self updateAppPath];
+      [self clearResultIndex];
+      [self indexScriptsForApp:app operation:operation];
+      if (![operation isCancelled]) {
+        [self saveResultsCache];
+      }
+    }
+    [self updateResultsCacheAfterDelay:60.0];
+  }
+}
+
+- (void)reloadResultsCache {
+  if ([self loadResultsCache]) {
+    [self updateResultsCacheAfterDelay:10.0];
+  } else {
+    [self updateResultsCache];
+  }
 }
 
 #pragma mark Result Generation
 - (void)indexScriptItem:(FastScriptsScriptItem *)script
    atPathWithComponents:(NSArray *)pathComponents
-{
-  NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-  [attrs setObject:script forKey:kFSScriptItemKey];
-  NSString *name = [script name];
-  NSString *snip = [NSString pathWithComponents:pathComponents];
-  NSString *path = [snip stringByAppendingPathComponent:name];
-  NSString *uri = [NSString stringWithFormat:kFSURIFormat,
-                   [path stringByAddingPercentEscapesUsingEncoding:
-                    NSUTF8StringEncoding]];
-  [attrs setObject:snip forKey:kHGSObjectAttributeSnippetKey];
-  if ([pathComponents count] > 1) {
-    if ([[pathComponents objectAtIndex:0] isEqualToString:@"Applications"]) {
-      [attrs setObject:[pathComponents objectAtIndex:1] forKey:kFSAppNameKey];
+              operation:(NSOperation *)operation {
+  if (![operation isCancelled]) {
+    NSString *name = [script name];
+    NSString *snip = [NSString pathWithComponents:pathComponents];
+    NSString *path = appPath_;
+    NSURL *file = [script scriptFile];
+    if (file) {
+      path = [file path];
     }
+    NSString *uri = _FSItemURI(name, snip);
+    NSString *app = _FSItemApp(pathComponents);
+    HGSAction *action = _FSItemAction();
+    NSDictionary *attrs = [NSDictionary dictionaryWithObjectsAndKeys:
+                           script, kFSScriptItemKey,
+                           action, kHGSObjectAttributeDefaultActionKey,
+                           snip, kHGSObjectAttributeSnippetKey,
+                           path, kHGSObjectAttributeIconPreviewFileKey,
+                           app, kFSAppNameKey,
+                           nil];
+    HGSResult *result = [HGSUnscoredResult resultWithURI:uri
+                                                    name:name
+                                                    type:kFSResultType
+                                                  source:self
+                                              attributes:attrs];
+    [self indexResult:result];
   }
-  NSURL *file = [script scriptFile];
-  NSImage *icon = appIcon_;
-  if (file)
-    icon = [[NSWorkspace sharedWorkspace] iconForFile:[file path]];
-  [attrs setObject:icon forKey:kHGSObjectAttributeIconKey];
-  HGSAction *action = [[HGSExtensionPoint actionsPoint]
-                       extensionWithIdentifier:kFSInvokeAction];
-  [attrs setObject:action forKey:kHGSObjectAttributeDefaultActionKey];
-  HGSResult *result = [HGSUnscoredResult resultWithURI:uri
-                                                  name:name
-                                                  type:kFSResultType
-                                                source:self
-                                            attributes:attrs];
-  [self indexResult:result];
 }
 
 - (void)indexScriptLibrary:(FastScriptsScriptLibrary *)library
       atPathWithComponents:(NSArray *)pathComponents
-{
-  for (FastScriptsScriptItem *script in [library scriptItems]) {
-    [self indexScriptItem:script atPathWithComponents:pathComponents];
-  }
-  for (FastScriptsScriptLibrary *sublibrary in [library scriptLibraries]) {
-    NSString *name = [sublibrary name];
-    NSArray *subpathComponents = [pathComponents arrayByAddingObject:name];
-    [self indexScriptLibrary:sublibrary atPathWithComponents:subpathComponents];
+                 operation:(NSOperation *)operation {
+  if (![operation isCancelled]) {
+    for (FastScriptsScriptItem *script in [library scriptItems]) {
+      [self indexScriptItem:script
+       atPathWithComponents:pathComponents
+                  operation:operation];
+    }
+    for (FastScriptsScriptLibrary *sublibrary in [library scriptLibraries]) {
+      NSString *name = [sublibrary name];
+      NSArray *subpathComponents = [pathComponents arrayByAddingObject:name];
+      [self indexScriptLibrary:sublibrary
+          atPathWithComponents:subpathComponents
+                     operation:operation];
+    }
   }
 }
 
-- (void)indexTopLevelScriptLibrary:(FastScriptsScriptLibrary *)library
-{
-  [self indexScriptLibrary:library atPathWithComponents:[NSArray array]];
+- (void)indexScriptsForApp:(FastScriptsApplication *)application
+                         operation:(NSOperation *)operation {
+  if (![operation isCancelled]) {
+    NSArray *topLevelScriptLibraries = [application topLevelScriptLibraries];
+    for (FastScriptsScriptLibrary *library in topLevelScriptLibraries) {
+      [self indexScriptLibrary:library
+          atPathWithComponents:[NSArray array]
+                     operation:operation];
+    }
+  }
 }
 
 #pragma mark Result Filtering
 - (BOOL)isResult:(HGSResult *)result
      validForApp:(NSString *)otherApp
-           orNil:(BOOL)nilOK
-{
+           orNil:(BOOL)nilOK {
+  BOOL valid = nilOK;
   NSString *scriptApp = [result valueForKey:kFSAppNameKey];
-  if (scriptApp == nil)
-    return nilOK;
-  return [scriptApp caseInsensitiveCompare:otherApp] == NSOrderedSame;
+  if (scriptApp) {
+    valid = [scriptApp caseInsensitiveCompare:otherApp] == NSOrderedSame;
+  }
+  return valid;
 }
 
 - (HGSResult *)preFilterResult:(HGSResult *)result
                matchesForQuery:(HGSQuery *)query
-                  pivotObjects:(HGSResultArray *)pivotObjects
-{
+                  pivotObjects:(HGSResultArray *)pivotObjects {
   BOOL valid = NO;
-  if (isEmpty(pivotObjects)) {
-    NSDictionary *activeApp = [[NSWorkspace sharedWorkspace] activeApplication];
-    NSString *appName = [activeApp objectForKey:@"NSApplicationName"];
-    valid = [self isResult:result validForApp:appName orNil:YES];
-  } else {
+  if ([pivotObjects count]) {
     for (HGSResult *pivot in pivotObjects) {
-      NSString *appName = [pivot displayName];
-      valid = valid || [self isResult:result validForApp:appName orNil:NO];
+      if (!valid) {
+        valid = [self isResult:result validForApp:[pivot displayName] orNil:NO];
+      }
     }
+  } else {
+    NSDictionary *appInfo = [[NSWorkspace sharedWorkspace] activeApplication];
+    NSString *appName = [appInfo objectForKey:@"NSApplicationName"];
+    valid = [self isResult:result validForApp:appName orNil:YES];
   }
   return valid ? result : nil;
 }
